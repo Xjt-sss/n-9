6522a11e2ba2f566436c6236b150c1682fa89367
Xjt-sss <146491748+Xjt-sss@users.noreply.github.com>
2023-11-09 19:34:46 -0300

    Add files via upload

---
+++ rocketnotes/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme.js'
+import { Details } from './pages/Detailes/index.jsx'
+import GlobalStyle from './styles/global.js'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Details />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
---
+++ rocketnotes/src/pages/Detailes/index.jsx
@@ -0,0 +1,16 @@
+import { Container } from "./style"
+import { Button } from '../../components/Button';
+
+
+export function Details() {
+
+  return (
+    <Container>
+      <h1>Hello Word!</h1>
+      <span>Rodriges Gonçauves</span>
+      <Button title="Entrar" loanding />
+      <Button title="Cadrastar"/>
+      <Button title="Voltar"/>
+    </Container>
+    )
+}
